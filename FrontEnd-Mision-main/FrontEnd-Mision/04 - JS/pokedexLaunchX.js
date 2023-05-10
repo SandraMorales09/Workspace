77bf46f1bcd3e691206843625f07af3e4874cfa1
SandraMorales09 <114303948+SandraMorales09@users.noreply.github.com>
2023-05-10 13:26:33 -0600

    Delete FrontEnd-Mision-main/FrontEnd-Mision directory

--- FrontEnd-Mision-main/FrontEnd-Mision/04 - JS/pokedexLaunchX.js
+++
@@ -1,9 +0,0 @@
-const fetchPokemon = () => {
-    const url = "http//pokeapi.co/api/v2/pokemon/chikorita";
-    fetch(url).then((res) => {
-        console.log(res);
-        return res.json();
-    })
-}
-fetchPokemon();
-
